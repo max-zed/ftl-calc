e1372bbe30771067dc67ede93b44bc688451fcca
max-zed <maximilian.zedschak@gmx.de>
2026-03-02 15:44:27 +0100

    fdp calc refinement

--- index.js
+++ index.js
@@ -42,17 +42,73 @@ const latestOnBlockElement = document.getElementById("on-block-time")
 
 function calculate_final_data() {
     var checkInTime = new Date(checkInTimeElement.value + "Z")
-    checkInTime.setHours(checkInTime.getHours() + 13) //Set maximum fdp time
-    var latestOnBlock = checkInTime
+    var latestOnBlock = new Date(checkInTime)
+    var maxFDP = latestOnBlock.getTime() - checkInTime.getTime()
+    console.log(maxFDP)
+
+    if (checkInTime.getUTCHours() >= 6 && (checkInTime.getUTCHours() < 13 || (checkInTime.getUTCHours() === 13 && checkInTime.getUTCMinutes() <= 29) )){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 13) //max fdp
+    }else if (checkInTime.getUTCHours() === 13 && (checkInTime.getUTCMinutes() >= 30 && checkInTime.getUTCMinutes() <= 59)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 45) 
+    }else if (checkInTime.getUTCHours() === 14 && checkInTime.getUTCMinutes() <= 29){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 30) 
+    }else if (checkInTime.getUTCHours() === 14 && (checkInTime.getUTCMinutes() >= 30 && checkInTime.getUTCMinutes() <= 59)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 15) 
+    }else if (checkInTime.getUTCHours() === 15 && checkInTime.getUTCMinutes() <= 29){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12) 
+    }else if (checkInTime.getUTCHours() === 15 && (checkInTime.getUTCMinutes() >= 30 && checkInTime.getUTCMinutes() <= 59)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 11)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 45) 
+    }else if (checkInTime.getUTCHours() === 16 && checkInTime.getUTCMinutes() <= 29){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 11)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 30) 
+    }else if (checkInTime.getUTCHours() === 16 && (checkInTime.getUTCMinutes() >= 30 && checkInTime.getUTCMinutes() <= 59)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 11)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 15) 
+    }else if (checkInTime.getUTCHours() >= 17 || checkInTime.getUTCHours() < 5){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 11)
+    }else if (checkInTime.getUTCHours() === 5 && checkInTime.getUTCMinutes() <= 14){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+    }else if (checkInTime.getUTCHours() === 5 && (checkInTime.getUTCMinutes() >= 15 && checkInTime.getUTCMinutes() <= 29)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 15)
+    }else if (checkInTime.getUTCHours() === 5 && (checkInTime.getUTCMinutes() >= 30 && checkInTime.getUTCMinutes() <= 44)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 30)
+    }else if (checkInTime.getUTCHours() === 5 && (checkInTime.getUTCMinutes() >= 45 && checkInTime.getUTCMinutes() <= 59)){
+        latestOnBlock.setUTCHours(latestOnBlock.getUTCHours() + 12)
+        latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() + 45)
+    }
+
+    
+    
+    
+
+
+    
 
     var legCount = legsElement.value
     if (legCount >= 3) {
         for (let i = 0; i < legCount - 2; i++){
-            latestOnBlock.setMinutes(latestOnBlock.getMinutes() - 30)
+            ///REWORK
+            if (i < 4){
+                latestOnBlock.setUTCMinutes(latestOnBlock.getUTCMinutes() - 30)
+            }else{
+                break
+            }
+            
         }
     }
     
+    maxFDP = latestOnBlock.getTime() - checkInTime.getTime()
+    const diffMinutes = Math.floor(maxFDP / 1000 / 60);
+    const diffHours = Math.floor(diffMinutes / 60);
+    const remainingMinutes = diffMinutes % 60;
 
+    console.log(diffHours+":"+remainingMinutes)
     
     const year = latestOnBlock.getUTCFullYear();
     const month = String(latestOnBlock.getUTCMonth() + 1).padStart(2, "0");
